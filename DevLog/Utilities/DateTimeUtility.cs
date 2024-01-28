12a6f5b7823883945ef595751677ca6307fc9d3b
Elliot One <ElliotOneOfficial@gmail.com>
2024-01-28 23:40:52 +0000

    Refactor date display in panel views to a friendly format

---
+++ DevLog/Utilities/DateTimeUtility.cs
@@ -0,0 +1,18 @@
+﻿namespace DevLog.Utilities
+{
+    /// <summary>
+    /// Represents DateTime utility
+    /// </summary>
+    public static class DateTimeUtility
+    {
+        /// <summary>
+        /// Convert a datetime to a user friendly representation; HH:mm:ss yyyy/MM/dd
+        /// </summary>
+        /// <param name="dateTime">DateTime</param>
+        /// <returns>DateTime in HH:mm:ss yyyy/MM/dd format</returns>
+        public static string ToFriendlyFullDateTime(this DateTime dateTime)
+        {
+            return dateTime.ToString("HH:mm:ss yyyy/MM/dd");
+        }
+    }
+}
